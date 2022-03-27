671e0d6d72c6cc38a0d5c39ad42f6db6a1331c8b
Danny Shen <shenfive@gmail.com>
2022-03-27 15:59:12 +0800

    get subjects

--- a202RTDB/Page2ViewController.swift
+++ a202RTDB/Page2ViewController.swift
@@ -6,16 +6,26 @@
 //
 
 import UIKit
+import Firebase
 
 class Page2ViewController: UIViewController {
     
     var nickname = ""
+    var ref:DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = "歡迎：\(nickname)"
-        // Do any additional setup after loading the view.
+        ref = Database.database().reference().child("for/subs")
+        ref.observeSingleEvent(of: .value) { dataSnap in
+            print(dataSnap)
+            for item in dataSnap.children{
+                print("=======")
+                print(item)
+            }
+        }
+        
     }
     
     
