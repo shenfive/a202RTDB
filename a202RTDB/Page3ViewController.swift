5e5a9b311bb1dae0714e8f8c94029f0f10959057
Danny Shen <shenfive@gmail.com>
2022-03-27 17:03:44 +0800

    new Msg

--- a202RTDB/Page3ViewController.swift
+++ a202RTDB/Page3ViewController.swift
@@ -6,29 +6,39 @@
 //
 
 import UIKit
+import Firebase
 
 class Page3ViewController: UIViewController {
     
     var key = ""
     var suject = ""
+    var ref:DatabaseReference!
 
+    @IBOutlet weak var theContentTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.title = "\(suject)"
+        ref = Database.database().reference().child("for/disc").child(key)
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func newMessage(_ sender: Any) {
+        let newMsg = theContentTF.text ?? ""
+        if newMsg.count < 1 {
+            let alert = UIAlertController(title: "至少輸入一個字", message: nil, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+            return
+        }
+        ref.childByAutoId().child("content").setValue(newMsg)
+        theContentTF.text = ""
+        
+        
+        
     }
-    */
+    
+
 
 }
