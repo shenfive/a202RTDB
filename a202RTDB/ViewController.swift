947105312200a48124e3b74a1f0a053d8ad987be
Danny Shen <shenfive@gmail.com>
2022-03-27 14:59:39 +0800

    types

--- a202RTDB/ViewController.swift
+++ a202RTDB/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var ref:DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Auth.auth().signInAnonymously(completion: nil)
+        ref = Database.database().reference()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        ref.child("test").child("string").setValue("這是一個測試")
+//
+//        var arr = [9999,333,444]
+//
+//        ref.child("test").child("array").setValue(arr)
+        
+        
+        var dic = ["name":"Danny","age":25] as [String : Any]
+        
+        ref.child("test").child("dic").setValue(dic)
     }
 
 
